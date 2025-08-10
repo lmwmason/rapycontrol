9645d874723589b107b168bd577aaede26581cfc
happy <lmwmason@naver.com>
2025-08-11 08:03:03 +0900

    create

---
+++ lib/__init__.py
@@ -0,0 +1,1 @@
+from stepper_control import *
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh :
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rapycontrol",
+    version="0.1.0",
+    author="Muwon Lee",
+    author_email="lmwmason@naver.com",
+    description="A Python library for controlling stepper motors and other devices.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lmwmason/rapycontrol",
+    
